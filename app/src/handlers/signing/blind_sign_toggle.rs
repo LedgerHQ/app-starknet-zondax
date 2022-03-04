3db61275b4578329d5076aeabc5c5788fd1fb5dc
becominginsane <francesco@zondax.ch>
2022-03-04 09:59:46 +0100

    tests(app): add mock for blind_sign for tests

--- app/src/handlers/signing/blind_sign_toggle.rs
+++ app/src/handlers/signing/blind_sign_toggle.rs
@@ -1,13 +1,25 @@
 #[cfg(feature = "blind-sign-toggle")]
-mod bindings {
+mod impls {
     #[repr(C)]
     pub struct BlindSignToggle {
         pub toggle: bool,
         pub message: [i8; 9],
     }
 
-    extern "C" {
-        pub static mut blind_sign: BlindSignToggle;
+    cfg_if::cfg_if! {
+        if #[cfg(any(unix, windows))] {
+            /// Provide a mock for tests
+            #[allow(non_upper_case_globals)]
+            pub static mut blind_sign: BlindSignToggle = BlindSignToggle {
+                toggle: true,
+                message: [0; 9],
+            };
+        } else {
+            extern "C" {
+                ///Link to the C code
+                pub static mut blind_sign: BlindSignToggle;
+            }
+        }
     }
 }
 
@@ -15,7 +27,7 @@ mod bindings {
 pub fn blind_sign_enabled() -> bool {
     if cfg!(feature = "blind-sign-toggle") {
         //safe: guaranteed no data races
-        unsafe { bindings::blind_sign.toggle }
+        unsafe { impls::blind_sign.toggle }
     } else {
         true //specific request of this app, only blind signing
     }
